be4b894166dc1eecb55131d7f152c519776f5241
Eduardo Vieira <edusvieirap@gmail.com>
2020-06-08 22:43:38 -0300

    Fix problems

--- Connection.py
+++ Connection.py
@@ -99,7 +99,7 @@ def get_annotations_by_video(video):
     mydb = init()
     mycursor = mydb.cursor()
 
-    mycursor.execute("select * from Annotations inner join Actors where Videos_code={} and Actors.code = Annotations.Actors_code order by Annotations.Actors_code".format(video))
+    mycursor.execute("select * from Annotations inner join Actors inner join Persons where Videos_code={} and Actors.code = Annotations.Actors_code order by Annotations.Actors_code".format(video))
 
     myresult = mycursor.fetchall()
 
